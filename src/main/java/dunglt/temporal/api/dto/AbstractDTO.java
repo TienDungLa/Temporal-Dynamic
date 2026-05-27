26bebda7292998c526c71c8044cdd87f0f63a348
dunglt <dunglt@sts.vn>
2026-05-27 09:18:45 +0700

    Fix: bug thừa field trong payload khi save vào inbox

--- src/main/java/dunglt/temporal/api/dto/AbstractDTO.java
+++ src/main/java/dunglt/temporal/api/dto/AbstractDTO.java
@@ -1,9 +1,13 @@
 package dunglt.temporal.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class AbstractDTO {
+    @JsonProperty("requestId")
     private String requestId;
+
+    @JsonProperty("sourceSystem")
     private String sourceSystem;
 }
--- src/main/java/dunglt/temporal/api/dto/DataDTO.java
+++ src/main/java/dunglt/temporal/api/dto/DataDTO.java
@@ -1,8 +1,10 @@
 package dunglt.temporal.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class DataDTO extends AbstractDTO {
-    private Object Data;
+    @JsonProperty("data")
+    private Object data;
 }
